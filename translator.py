1a5716874db6d7c3c257e9de725511978a1bd8cf
Conor <conor4508@gmail.com>
2020-06-08 18:01:28 -0400

    made the funny feature

--- translator.py
+++ translator.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,9 +7,10 @@ import time
 import random
 
 class Translation_Gag:
+    
     def __init__(self, word, iterations):
         self.word = word
-        self.iter = iterations
+        self.iter = int(iterations)
         self.chrome_options = Options()
 
         self.chrome_options.add_argument("--disable-extensions")
@@ -23,10 +23,10 @@ class Translation_Gag:
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options = self.chrome_options)
         self.driver.get("https://translate.google.com/")
         time.sleep(2)
-        print("[" + start_lang + "]"  + word)
+        print("[" + self.start_lang + "]"  + self.word)
         for x in range(0, self.iter):
-            self.word = main_loop(self.word)
-        end_loop(self.word)  
+            self.word = self.main_loop(self.word)
+        self.end_loop(self.word)  
 
 
     def build_lang_list(self):
@@ -47,59 +47,59 @@ class Translation_Gag:
         
     def select_language(self):
         self.lang_out = self.select_lang(self.langs, self.lang_in)
-        menu = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
+        menu = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
         menu.send_keys(Keys.RETURN)
-        option = driver.find_element_by_xpath(opt_path + self.langs[self.lang_out] + ']')
+        option = self.driver.find_element_by_xpath(self.opt_path + self.langs[self.lang_out] + ']')
         option.send_keys(Keys.RETURN)
         self.lang_in = self.lang_out
         return self.lang_out
 
     def force_language(self):
         self.lang_out = 'English'
-        menu = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
+        menu = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
         menu.send_keys(Keys.RETURN)
-        option = driver.find_element_by_xpath(opt_path + self.langs[self.lang_out] + ']')
+        option = self.driver.find_element_by_xpath(self.opt_path + self.langs[self.lang_out] + ']')
         option.send_keys(Keys.RETURN)
         self.lang_in = 'English'
         return self.lang_out
 
-def retranslate(self):
-    self.lang_out = self.start_lang
-    menu = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
-    menu.send_keys(Keys.RETURN)
-    option = driver.find_element_by_xpath(opt_path + self.langs[self.lang_out] + ']')
-    option.send_keys(Keys.RETURN)
-    self.lang_in = self.lang_out
-    return self.lang_out
+    def retranslate(self):
+        self.lang_out = self.start_lang
+        menu = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]")
+        menu.send_keys(Keys.RETURN)
+        option = self.driver.find_element_by_xpath(self.opt_path + self.langs[self.lang_out] + ']')
+        option.send_keys(Keys.RETURN)
+        self.lang_in = self.lang_out
+        return self.lang_out
 
-def get_output(self, word):
-    enter = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/textarea")
-    enter.clear()
-    enter.send_keys(word)
-    time.sleep(7)
-    exit = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/div/span[1]")
-    return exit.text
+    def get_output(self, word):
+        enter = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/textarea")
+        enter.clear()
+        enter.send_keys(word)
+        time.sleep(7)
+        exit = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/div/span[1]")
+        return exit.text
 
-def main_loop(self, word):
-    lan = self.select_language()
-    out = self.get_output(word)
-    print('-->'  + out)
-    switch = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div")
-    time.sleep(7)
-    switch.send_keys(Keys.RETURN)
-    return out
+    def main_loop(self, word):
+        lan = self.select_language()
+        out = self.get_output(word)
+        print('-->'  + out)
+        switch = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div")
+        time.sleep(7)
+        switch.send_keys(Keys.RETURN)
+        return out
 
-def mod_loop(self, word):
-    lan = self.force_language()
-    out = self.get_output(word)
-    print('-->'  + out)
-    switch = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div")
-    time.sleep(5)
-    switch.send_keys(Keys.RETURN)
-    return out
+    def mod_loop(self, word):
+        lan = self.force_language()
+        out = self.get_output(word)
+        print('-->'  + out)
+        switch = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div")
+        time.sleep(5)
+        switch.send_keys(Keys.RETURN)
+        return out
 
-def end_loop(self, word):
-    lan = self.retranslate()
-    out = self.get_output(word)
-    print('[' + lan + ']' + out)
+    def end_loop(self, word):
+        lan = self.retranslate()
+        out = self.get_output(word)
+        print('[' + lan + ']' + out)
 
